477d15412d121e98b930f4eea613dec189f264f9
Lewluu <iuliuantoniu@gmail.com>
2023-06-25 18:43:09 +0300

    testing requests and damn it works!

--- php/server.php
+++ php/server.php
@@ -1,4 +1,9 @@
 <?php
     header('Access-Control-Allow-Origin: http://localhost:3000');
-    echo ("Hello from server!");
+    // echo(json_encode("Test PHP... IT WORKS !!!"));
+
+    $data = "Test PHP ... IT WORKS !!! ".($_POST["json_data"] + 2);
+
+    echo(json_encode($data));
+    // echo ("Test PHP... IT WORKS !!!");
 ?>
--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import "./App.css";
   
-class App extends React.Component{
+function App() {
 
-    constructor(props){
-        super(props);
+    var jsonData = 5;
+    var formData = new FormData();
+    formData.append("json_data", JSON.stringify(jsonData))
 
-        this.state = {
-            postId: null
-        };
-    }    
+    fetch("http://localhost/josphere/php/server.php",{
+        method: "POST",
+        body: formData
+    })
+        .then((response) => response.json())
+        .then((data)=>{
+            console.log(data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
 
-    componentDidMount() {
-        // Simple POST request with a JSON body using fetch
-        const requestOptions = {
-            method: 'POST',
-            // headers: { 'Content-Type': 'application/json' },
-            // body: JSON.stringify({ title: 'React POST Request Example' })
-        };
-        fetch('http://localhost/josphere/php/server.php', requestOptions)
-            .then(response => response.text(), requestOptions)
-            .then(text => {
-                console.log(text)
-            });
-    }
-
-    render(){
-
-        // const { postId } = this.state;
-
-        return (
-            <div>
-                {/* Test post id: {postId} */}
-            </div>
-        );
-  }}
+    return (
+      <div className="App">
+      </div>
+    );
+  }
   
-export default App;
+  export default App;
